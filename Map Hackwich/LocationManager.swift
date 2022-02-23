cd2a7ed220d7b9ac61a8a7d03fbe780a70ae136d
Awhite246 <awhite@bsd220.org>
2022-02-23 10:44:33 -0600

    Added geocoded lookup

--- Map Hackwich/LocationManager.swift
+++ Map Hackwich/LocationManager.swift
@@ -10,10 +10,12 @@ import CoreLocation
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     var locationManager = CLLocationManager()
+    var geocoder = CLGeocoder()
     override init() {
         super.init()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
+    
 }
